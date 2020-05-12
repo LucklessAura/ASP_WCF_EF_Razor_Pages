f37f05096ffdc0cb46730879294e65efb4efad38
LucklessAura <paul.curea.andrei@gmail.com>
2020-05-12 14:12:21 +0300

    initial commit

---
+++ PostComment/RazorPagesPostComment/Models/CommentDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace RazorPagesPostComment.Models
+{
+    [DataContract(IsReference = true)]
+    public partial class CommentDTO
+    {
+        [DataMember]
+        public int CommentId { get; set; }
+        [DataMember]
+        public string Text { get; set; }
+        [DataMember]
+        public int PostPostId { get; set; }
+        [DataMember] public virtual PostDTO Post { get; set; }
+    }
+}
